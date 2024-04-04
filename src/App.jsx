11f324b6ca7a5c0866dfe28937dc6d4cda5b32bc
AdityaKannur <adityakannur28@gmail.com>
2024-04-04 10:50:03 +0530

    redux-thunk

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Counter from './Components/Counter'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Counter/>
+    </>
+  )
+}
+
+export default App
